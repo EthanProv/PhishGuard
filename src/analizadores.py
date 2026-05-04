6fbe206bd2b37ead3a25d988e58c09b1fe824e62
EthanProv <ethan.provencio@estudiant.enti.cat>
2026-05-04 21:58:41 +0200

    Nuevo mdoelo

---
+++ src/analizadores.py
@@ -0,0 +1,10 @@
+'''
+Para el polimorfismo:
+
+Todos los analizadores tendrán un método común:
+
+analizar(correo, resultado)
+
+Pero cada clase lo hará de forma diferente.
+'''
+
---
+++ src/correo.py
@@ -0,0 +1,14 @@
+'''
+Representa un correo electrónico.
+
+Debe guardar:
+
+asunto,
+cuerpo,
+remitente,
+URLs,
+adjuntos,
+cabeceras,
+respuestas anidadas.
+'''
+
