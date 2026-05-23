1803e38a5622a81f51761afce6fe25c8fd132552
jhoanlenti <jhoan.lope@enti.cat>
2026-05-23 11:58:20 +0200

    Update models.py

--- SOURCE/models.py
+++ SOURCE/models.py
@@ -4,26 +4,26 @@ class Email:
     """Representa un mensaje de correo con posibles respuestas anidadas."""
  
     def __init__(self, email_id, headers, subject, body, attachments):
-        self.email_id = email_id
-        self.headers = headers
-        self.subject = subject
-        self.body = body
-        self.attachments = attachments
-        self.replies = []
+        self.email_id = email_id                                             #O(1)
+        self.headers = headers                                               #O(1)
+        self.subject = subject                                               #O(1)
+        self.body = body                                                     #O(1)
+        self.attachments = attachments                                       #O(1)
+        self.replies = []                                                    #O(1)
  
     def add_reply(self, reply):
         """Añade una respuesta al correo actual."""
-        self.replies.append(reply)
+        self.replies.append(reply)                                           #O(1)
  
     def print_thread(self, level=0):
         """
         Muestra el hilo de correos usando recursividad.
         Complejidad: O(n), porque recorre todos los correos del hilo.
         """
-        spaces = "    " * level
-        print(f"{spaces}[{self.email_id}] {self.subject}")
+        spaces = "    " * level                                              #O(1)
+        print(f"{spaces}[{self.email_id}] {self.subject}")                   #O(1)
  
-        for reply in self.replies:
-            reply.print_thread(level + 1)
+        for reply in self.replies:                                           #O(N)
+            reply.print_thread(level + 1)                                    #O(1)
  
 #T(n) = 5 + 6 +2 + (1+2+1n) --> T(n)= 12 + (3+n)
